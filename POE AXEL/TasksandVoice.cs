5fd46fb66d451f370771f5772f0171f604e95e01
IsamEltawil <St10457643@rcconnect.edu.za>
2025-04-01 15:51:12 +0200

    third commit

--- POE AXEL/TasksandVoice.cs
+++ POE AXEL/TasksandVoice.cs
@@ -9,6 +9,14 @@ namespace POE_AXEL
             Console.Write("Enter your name: ");
             string name = Console.ReadLine();
             Console.WriteLine($"Welcome, {name}! Let's talk about cybersecurity.");
+
+            Console.WriteLine("\nChoose a topic:");
+            Console.WriteLine("1. Password Safety");
+            Console.WriteLine("2. Phishing");
+            Console.WriteLine("3. Safe Browsing");
+
+            int choice = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"You selected option {choice}.");
         }
     }
 }
